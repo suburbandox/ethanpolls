60407325892425310e72c5d4f0e69c27c56c1c84
suburbandox <emkline@gmail.com>
2023-11-25 12:04:32 -0600

    added coluns to admin

--- polls/admin.py
+++ polls/admin.py
@@ -3,5 +3,8 @@ from django.contrib import admin
 
 from .models import Question,Choice
 
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ["question", "choice_text"]
+
 admin.site.register(Question)
-admin.site.register(Choice)
+admin.site.register(Choice,ChoiceAdmin)
