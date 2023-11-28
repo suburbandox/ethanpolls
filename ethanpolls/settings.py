9ee6e584907444cd8329bdc563974871ad3121b8
Ethan Kline <emkline@gmail.com>
2023-11-28 12:45:43 -0600

    fin

--- ethanpolls/settings.py
+++ ethanpolls/settings.py
@@ -10,9 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import mimetypes
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 mimetypes.add_type("application/javascript",".js")
-
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xc7f$!-#d9@z1e*5w=k^m=ek&#f!cg-r9-_-pn-yh60r55xf1l'
+SECRET_KEY =os.environ['DJANGO_SECRETKEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ['DJANGO_DEBUG']  == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
