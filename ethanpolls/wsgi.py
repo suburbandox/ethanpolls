becdde1d9d7f53685d2f0a5081a6874e1db975b4
Ethan Kline <emkline@gmail.com>
2023-11-28 13:39:52 -0600

    wsggh

--- ethanpolls/wsgi.py
+++ ethanpolls/wsgi.py
@@ -9,8 +9,14 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 
 import os
 import sys
+from dotenv import load_dotenv
+
 from django.core.wsgi import get_wsgi_application
 sys.path.append("/home/emkline/ethanpolls")
+project_folder = os.path.expanduser('~/ethampolls')  
+load_dotenv(os.path.join(project_folder, '.env'))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ethanpolls.settings')
 
 application = get_wsgi_application()
+
+
