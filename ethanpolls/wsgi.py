face21655b9128f1e63ca870695c85ab7d82e90a
Ethan Kline <emkline@gmail.com>
2023-11-28 13:45:08 -0600

    fffffffffffffffffff

--- ethanpolls/wsgi.py
+++ ethanpolls/wsgi.py
@@ -13,7 +13,7 @@ from dotenv import load_dotenv
 
 from django.core.wsgi import get_wsgi_application
 sys.path.append("/home/emkline/ethanpolls")
-project_folder = os.path.expanduser('~/ethampolls')  
+project_folder = os.path.expanduser('~/ethanpolls')  
 load_dotenv(os.path.join(project_folder, '.env'))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ethanpolls.settings')
 
