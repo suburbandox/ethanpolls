3c4d51b2284d1bc07581b72f512f271508bf06b8
Ethan Kline <emkline@gmail.com>
2023-11-28 13:28:48 -0600

    add python dot env

--- ethanpolls/wsgi.py
+++ ethanpolls/wsgi.py
@@ -8,9 +8,9 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
-
+sys.path.append("/home/emkline/ethanpolls")
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ethanpolls.settings')
 
 application = get_wsgi_application()
