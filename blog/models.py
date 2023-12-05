af1b83aa8febf1647c17760f3ae4e46130dea910
Razzi Abuissa <razzi@abuissa.net>
2023-12-05 16:43:54 -0600

    Add last_updated to BlogPost

--- blog/models.py
+++ blog/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 # Create your models here.
 class BlogPost(models.Model):
-    date = models.DateField()
+    date = models.DateTimeField()
     title = models.CharField(max_length=100)
     textarea =models.TextField()
+    last_updated = models.DateTimeField(null=True)
 
     def __str__(self):
         return self.title
