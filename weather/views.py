25c56dc94f593544d68c6e79ca04bd59d279e2b4
suburbandox <emkline@gmail.com>
2023-12-18 20:19:19 -0600

    fix windows 10 bug

--- weather/views.py
+++ weather/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 import urllib
 import json
+import ssl
+import requests
 
 def weather_index(request):
     
@@ -8,10 +10,12 @@ def weather_index(request):
         rat =request.GET["rat"]
         atooi =request.GET["atooi"]
         url = f"https://api.open-meteo.com/v1/forecast?latitude={rat}&longitude={atooi}&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m" 
-        response = urllib.request.urlopen(url).read()
-        data = json.loads(response)
+        # response = urllib.request.urlopen(url,context=ssl._create_unverified_context()).read()
+        response = requests.get(url)
+        data = response.json()
         celsius = data['current']["temperature_2m"]
-        fahrenheit= celsius *1.8 +32 
+        fahrenheit= celsius *1.8 +32
+        print(celsius) 
         context = {'temp':fahrenheit}
     else:
         context = {}
